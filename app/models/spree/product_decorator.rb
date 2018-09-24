10cc0edaaa97b7d771ad01fffa98c2a7a54f35eb
dylanfisher <hi@dylanfisher.com>
2018-09-24 00:10:02 -0400

    Replace product delegate_belongs_to with regular delegate

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Product.class_eval do
-  delegate_belongs_to :master, :sale_price
+  delegate :sale_price, to: :master
+  # delegate_belongs_to :master, :sale_price
   #attr_accessible :sale_price
 
   def on_sale?
