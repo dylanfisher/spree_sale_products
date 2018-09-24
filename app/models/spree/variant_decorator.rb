341e7fb5af240d380cfdc42745a2ca34126874f2
dylanfisher <hi@dylanfisher.com>
2018-09-24 00:10:15 -0400

    Always make sure a sale price is set to 0 when saving a variant

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,8 +1,15 @@
 Spree::Variant.class_eval do
   #attr_accessible :sale_price
+  before_validation { |record| record.sale_price = 0 if record.sale_price.nil? }
+
+  alias_method :orig_price_in, :price_in
+  def price_in(currency)
+    return orig_price_in(currency) unless on_sale?.present?
+    Spree::Price.new(variant_id: self.id, amount: self.sale_price, currency: currency)
+  end
 
   def on_sale?
-    sale_price > 0.0
+    sale_price.present? && sale_price > 0.0
   end
 
   def sale_perc
@@ -10,6 +17,6 @@ Spree::Variant.class_eval do
       discount = (1 - (self.sale_price / self.price)) * 100
       discount.round
     end
-  end 
-  
+  end
+
 end
